f754579887eafab5fdd195bc0c724648bd2630ee
4Rinzler4 <127138464+4Rinzler4@users.noreply.github.com>
2024-10-11 22:40:02 +0300

    updated headdphones.css

--- scripts/headphones.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const emblaNode = document.querySelector('.headphones__catalog');
-    const embla = EmblaCarousel(emblaNode, {
-      loop: true,          
-      draggable: true,     
-      speed: 4             
-    });
-  });
-  
