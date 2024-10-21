51ba0a8a05d3d66d475ef9e6024dca2c9d4f2108
Kirasin225 <108300947+Kirasin225@users.noreply.github.com>
2024-10-21 17:02:16 +0300

    add my solutions for codewars tasks in  assignments.js

--- codewars/assignments.js
+++ codewars/assignments.js
@@ -10,7 +10,10 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 // solution | Yehor Honcharenko
 const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) =>
     laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-
+// solution | Diakov Roman
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return Number(laLigaGoals) + Number(copaDelReyGoals) + Number(championsLeagueGoals);
+}
 
 // Make negative https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 // solution | Andrii Ferenchuk
@@ -28,6 +31,14 @@ function makeNegative(num) {
 function makeNegative(num) {
     return num > 0 ? num - (num * 2) : num;
   }
+// solution | Diakov Roman
+function makeNegative(num) {
+  if (num <= 0) {
+    return num;
+  } else {
+    return -num;
+  }
+}
 
 // Game Move  https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 // solution | Andrii Ferenchuk
@@ -41,6 +52,10 @@ function move (position, roll) {
 // solution | Yehor Honcharenko
 const move = (position, roll) =>
     position + roll*2;
+// solution | Diakov Roman
+function move(position, roll) {
+  return Number(position) + Number(roll) * 2;
+}
 
 // Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 // solution | Andrii Ferenchuk
@@ -58,6 +73,14 @@ if (name === owner) {
 // solution | Yehor Honcharenko
 const greet = (name,owner) =>
     name === owner ? "Hello boss" : "Hello guest";
+// solution | Diakov Roman
+function greet(name, owner) {
+  if (name === owner) {
+    return "Hello boss";
+  } else {
+    return "Hello guest";
+  }
+}
 
 // Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 // solution | Andrii Ferenchuk
@@ -72,6 +95,10 @@ function litres(time) {
 // solution | Yehor Honcharenko
 const litres = (time) =>
     Math.floor(time * 0.5);
+// solution | Diakov Roman
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
 
 // Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 // solution | Andrii Ferenchuk
@@ -85,4 +112,7 @@ function areTheyInLove(flower1, flower2) {
 // solution | Yehor Honcharenko
 const lovefunc = (flower1, flower2) =>
     flower1 % 2 !== flower2 % 2;
-
+// solution | Diakov Roman
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
+}
