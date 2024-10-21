07327df6394ca9d1c463f38759f65eda6f8e2b78
yehor.design <yehor.design@gmail.com>
2024-10-21 18:58:46 +0200

    update: js-tasks

---
+++ js-tasks/tasks-02.js
@@ -0,0 +1,7 @@
+// Convert a string to an array https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+// DNA to RNA Conversion https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+// Find Maximum and Minimum Values of a List https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+// Smallest value of an array https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
