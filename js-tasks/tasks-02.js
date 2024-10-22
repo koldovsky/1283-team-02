bdfe4327276d6c80799cb172170dd3de6e40efae
koskin <118272123+koskin17@users.noreply.github.com>
2024-10-22 13:18:43 +0300

    Zeykin add solutions for tasks-02

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,7 +1,31 @@
 // Convert a string to an array https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+// Kostia Zeykin
+function stringToArray(string) {
+  return string.split(" ");
+}
 
 // DNA to RNA Conversion https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+// Kostia Zeykin
+function DNAtoRNA(dna) {
+  return dna.includes("T") ? dna.prototype.replaceAll("T", "U") : dna;
+}
 
 // Find Maximum and Minimum Values of a List https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+// Kostia Zeykin
+var min = function(list){
+    function sortForMin(a, b) {
+      return a > b ? 1 : b > a ? -1 : 0;
+    }
+    list = list.sort(sortForMin);
+    return list[0];
+}
 
 // Smallest value of an array https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+// Kostia Zeykin
+function min(arr, toReturn) {
+  if (toReturn === "value") {
+    return Math.min(...arr);
+  } else {
+    return arr.indexOf(Math.min(...arr));
+  }
+}
