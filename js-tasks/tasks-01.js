e2b755528909c44431f9303d0d801a49a0a2f068
kriLEBED <47117380+kriLEBED@users.noreply.github.com>
2024-10-23 08:49:37 +0000

    add task 01 js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -14,6 +14,10 @@ const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) =>
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return Number(laLigaGoals) + Number(copaDelReyGoals) + Number(championsLeagueGoals);
 }
+// solution | Krystyna Lebid
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return (laLigaGoals + copaDelReyGoals + championsLeagueGoals);
+}
 
 // Make negative https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 // solution | Andrii Ferenchuk
@@ -29,6 +33,14 @@ function makeNegative(num) {
     return num > 0 ? num - (num * 2) : num;
   }
 // solution | Diakov Roman
+function makeNegative(num) {
+  if (num <= 0) {
+    return num;
+  } else {
+    return -num;
+  }
+}
+// solution | Krystyna Lebid
 function makeNegative(num) {
   if (num <= 0) {
     return num;
@@ -53,6 +65,10 @@ const move = (position, roll) =>
 function move(position, roll) {
   return position + roll * 2;
 }
+// solution | Krystyna Lebid
+function move (position, roll) {
+  return position + (roll * 2);
+}
 
 // Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 // solution | Andrii Ferenchuk
@@ -69,6 +85,14 @@ const greet = (name,owner) =>
     name === owner ? "Hello boss" : "Hello guest";
 // solution | Diakov Roman
 function greet(name, owner) {
+  if (name === owner) {
+    return "Hello boss";
+  } else {
+    return "Hello guest";
+  }
+}
+// solution | Krystyna Lebid
+function greet (name, owner) {
   if (name === owner) {
     return "Hello boss";
   } else {
@@ -93,6 +117,10 @@ const litres = (time) =>
 function litres(time) {
   return Math.floor(time * 0.5);
 }
+// solution | Krystyna Lebid
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
 
 // Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 // solution | Andrii Ferenchuk
@@ -109,4 +137,8 @@ const lovefunc = (flower1, flower2) =>
 // solution | Diakov Roman
 function lovefunc(flower1, flower2) {
   return flower1 % 2 !== flower2 % 2;
+}
+// solution | Krystyna Lebid
+function lovefunc(flower1, flower2){
+  return ((flower1 + flower2) % 2 !== 0);
 }
