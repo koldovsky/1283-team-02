0045a3120aad09ea322625dfc871b049f35e5301
4Rinzler4 <127138464+4Rinzler4@users.noreply.github.com>
2024-10-24 02:14:53 +0300

    headphones.css updated

--- scripts/headphones.js
+++ scripts/headphones.js
@@ -1,22 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const checkEmbla = () => {
-      const emblaNode = document.querySelector(".embla__viewport");
-      const prevButton = document.querySelector(".embla__arrow-btn--left");
-      const nextButton = document.querySelector(".embla__arrow-btn--right");
-  
-      if (emblaNode) {
-        const options = { align: "start", loop: true };
-        const emblaApi = EmblaCarousel(emblaNode, options);
-  
-        prevButton.addEventListener("click", () => emblaApi.scrollPrev());
-        nextButton.addEventListener("click", () => emblaApi.scrollNext());
-  
-        console.log(emblaApi.slideNodes()); 
-      } else {
-        setTimeout(checkEmbla, 100); 
-      }
-    };
-  
-    checkEmbla(); 
-  });
-  
+  const checkCarousel = () => {
+    const carouselNode = document.querySelector(".embla__container"); 
+    const prevButton = document.querySelector(".embla__arrow-btn--left");
+    const nextButton = document.querySelector(".embla__arrow-btn--right");
+
+    if (carouselNode) {
+      const options = { align: "start", loop: true };
+      const emblaApi = EmblaCarousel(carouselNode, options); 
+      prevButton.addEventListener("click", () => emblaApi.scrollPrev());
+      nextButton.addEventListener("click", () => emblaApi.scrollNext());
+
+      console.log(emblaApi.slideNodes()); 
+      setTimeout(checkCarousel, 100);
+    }
+  };
+
+  checkCarousel(); 
+});
