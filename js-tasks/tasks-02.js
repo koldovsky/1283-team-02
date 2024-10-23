b4f6e1d66e81ccc7710afa2d9863405dced2f266
koskin <118272123+koskin17@users.noreply.github.com>
2024-10-23 13:08:02 +0300

    Zeykin add solutions of tasks in tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -52,3 +52,45 @@ function min(arr, toReturn) {
     return arr.indexOf(minValue);
   }
 }
+
+// Twice as old https://www.codewars.com/kata/5b853229cfde412a470000d0/solutions/javascript
+// Kostia Zeykin
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - (sonYearsOld * 2))
+}
+
+// Get Nth Even Number https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+// kostia Zeykin
+function nthEven(n){
+  return (n * 2) - 2
+}
+
+// What's the real floor? https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+// Kostia Zeykin
+function getRealFloor(n) {
+  if (n <= 0) {
+    return n;
+  } else if (n < 13) {
+    return n - 1;
+  } else {
+    return n - 2;
+  }
+}
+
+// Beginner Series #2 Clock https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+// Kostia Zeykin
+function past(h, m, s) {
+  const hoursToMilliseconds = h * 3600000;
+  const minutesToMilliseconds = m * 60000;
+  const secondsToMilliseconds = s * 1000;
+  return hoursToMilliseconds + minutesToMilliseconds + secondsToMilliseconds;
+}
+
+// Is n divisible by x and y? https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+// Kostia Zeykin
+function isDivisible(n, x, y) {
+  if (n % x === 0 && n % y === 0) {
+    return true;
+  } else {
+    return false;
+  }
