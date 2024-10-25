1718fa01c4bc21001c43ca16ec5eafcf6c5d1675
4Rinzler4 <127138464+4Rinzler4@users.noreply.github.com>
2024-10-25 15:44:25 +0300

    changes headphone section

--- scripts/headphones.js
+++ scripts/headphones.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   const checkCarousel = () => {
-    const carouselNode = document.querySelector(".embla__container"); 
+    const carouselNode = document.querySelector(".embla__viewport"); 
     const prevButton = document.querySelector(".embla__arrow-btn--left");
     const nextButton = document.querySelector(".embla__arrow-btn--right");
-
+    
     if (carouselNode) {
       const options = { align: "start", loop: true };
       const emblaApi = EmblaCarousel(carouselNode, options); 
       prevButton.addEventListener("click", () => emblaApi.scrollPrev());
       nextButton.addEventListener("click", () => emblaApi.scrollNext());
 
-      console.log(emblaApi.slideNodes()); 
+      console.log(emblaApi.slideNodes());
+    } else { 
       setTimeout(checkCarousel, 100);
     }
   };
