249bc507ce0b98a4c79d02e71a2b7ec91d25ac68
Andrii Ferenchuk <ferenchuk.a@hotmail.com>
2024-10-26 20:04:01 +0300

    refactor js code

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -8,13 +8,14 @@ function findSmallestInt(arr) {
 // Andrii Ferenchuk
 function circleCircumference(circle) {
     const circumference = circle.radius * Math.PI * 2;
-    return circumference;
+    return circumference.toFixed(6);
 }
 https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
 // Andrii Ferenchuk
 function giveMeFive(obj) {
     const array = []
-    for (const key in obj) {        
+    for (const key in obj) {
+        console.log(key);
         if (key.length === 5) {
             array.push(key)
         }
