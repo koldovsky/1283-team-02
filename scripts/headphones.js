eb04c9566f3d14418ad5a7d04853711e91462681
4Rinzler4 <127138464+4Rinzler4@users.noreply.github.com>
2024-10-29 00:12:33 +0200

    fix headphones  carousel

--- scripts/headphones.js
+++ scripts/headphones.js
@@ -9,18 +9,20 @@ const checkCarousel = () => {
     const repeatCount = 10;
 
     for (let i = 0; i < repeatCount; i++) {
-      const startClones = slides.map(slide => slide.cloneNode(true));
-      const endClones = slides.map(slide => slide.cloneNode(true)).reverse();
+      const startClones = slides.map((slide) => slide.cloneNode(true));
+      const endClones = slides.map((slide) => slide.cloneNode(true)).reverse();
 
-      endClones.forEach(clone => slidesContainer.insertBefore(clone, slidesContainer.firstChild));
-      startClones.forEach(clone => slidesContainer.appendChild(clone));
+      endClones.forEach((clone) =>
+        slidesContainer.insertBefore(clone, slidesContainer.firstChild),
+      );
+      startClones.forEach((clone) => slidesContainer.appendChild(clone));
     }
 
     const options = { align: "start", loop: false };
     const emblaApi = EmblaCarousel(carouselNode, options);
 
     emblaApi.on("init", () => {
-      emblaApi.scrollTo(slides.length * repeatCount, false); 
+      emblaApi.scrollTo(slides.length * repeatCount, false);
     });
 
     prevButton.addEventListener("click", () => emblaApi.scrollPrev());
@@ -31,5 +33,5 @@ const checkCarousel = () => {
     setTimeout(checkCarousel, 100);
   }
 };
-
+checkCarousel();
 
