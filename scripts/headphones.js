7a8fa284a770d599c4db7ebbb01f23c6cab1d2af
4Rinzler4 <emaillubomyr103@gmail.com>
2024-10-29 12:37:13 +0200

    add working timer in headphones.js

--- scripts/headphones.js
+++ scripts/headphones.js
@@ -35,3 +35,51 @@ const checkCarousel = () => {
 };
 checkCarousel();
 
+const countdownContainers = document.querySelectorAll(".product-card__timer");
+
+const currentYear = new Date().getFullYear();
+
+const border1FinalDate = new Date(`February 25, ${currentYear + 1} 00:00:00`);
+const border2FinalDate = new Date(`March 15, ${currentYear + 1} 00:00:00`);
+const border3FinalDate = new Date(`May 28, ${currentYear + 1} 00:00:00`);
+
+function updateCountdown() {
+  const currentTime = new Date();
+  
+  const diffBorder1 = border1FinalDate - currentTime;
+  const diffBorder2 = border2FinalDate - currentTime;
+  const diffBorder3 = border3FinalDate - currentTime;
+  
+  const daysLeftBorder1 = Math.floor(diffBorder1 / 1000 / 60 / 60 / 24);
+  const hoursLeftBorder1 = Math.floor(diffBorder1 / 1000 / 60 / 60) % 24;
+  const minutesLeftBorder1 = Math.floor(diffBorder1 / 1000 / 60) % 60;
+
+  const daysLeftBorder2 = Math.floor(diffBorder2 / 1000 / 60 / 60 / 24);
+  const hoursLeftBorder2 = Math.floor(diffBorder2 / 1000 / 60 / 60) % 24;
+  const minutesLeftBorder2 = Math.floor(diffBorder2 / 1000 / 60) % 60;
+
+  const daysLeftBorder3 = Math.floor(diffBorder3 / 1000 / 60 / 60 / 24);
+  const hoursLeftBorder3 = Math.floor(diffBorder3 / 1000 / 60 / 60) % 24;
+  const minutesLeftBorder3 = Math.floor(diffBorder3 / 1000 / 60) % 60;
+
+  countdownContainers.forEach((container, index) => {
+    switch(index) {
+      case 0:
+        container.querySelector(".timer__days").innerText = daysLeftBorder1;
+        container.querySelector(".timer__hours").innerText = hoursLeftBorder1;
+        container.querySelector(".timer__minutes").innerText = minutesLeftBorder1;
+        break;
+      case 1:
+        container.querySelector(".timer__days").innerText = daysLeftBorder2;
+        container.querySelector(".timer__hours").innerText = hoursLeftBorder2;
+        container.querySelector(".timer__minutes").innerText = minutesLeftBorder2;
+        break;
+      case 2:
+        container.querySelector(".timer__days").innerText = daysLeftBorder3;
+        container.querySelector(".timer__hours").innerText = hoursLeftBorder3;
+        container.querySelector(".timer__minutes").innerText = minutesLeftBorder3;
+        break;
+    }
+  });
+}
+setInterval(updateCountdown, 1000);
