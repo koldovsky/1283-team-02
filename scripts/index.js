6d4c76fe158ca2575bcdf3fa7fb72ccde7c37110
koskin <118272123+koskin17@users.noreply.github.com>
2024-10-30 13:48:31 +0200

    add gallery of speakers on index page

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,18 @@
 function init() {
   import("./header.js");
-  import("./product-tabs.js");  
+  import("./product-tabs.js");
   import("./headphones.js");
   import("./out-customers-say.js");
-  import("./special-offers.js")
+  import("./special-offers.js");
+  import("./index.speakers.carousel.js");
 }
 
-const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+const totalPartials = document.querySelectorAll(
+  '[hx-trigger="load"], [data-hx-trigger="load"]'
+).length;
 let loadedPartialsCount = 0;
 
-document.body.addEventListener('htmx:afterOnLoad', () => {
+document.body.addEventListener("htmx:afterOnLoad", () => {
   loadedPartialsCount++;
   if (loadedPartialsCount === totalPartials) init();
 });
---
+++ scripts/index.speakers.carousel.js
@@ -0,0 +1,93 @@
+/* Create a list of slides */
+
+const slides = [
+  '<article class="speakers__card">\
+        <img class="speakers__card-image" src="assets/images/speakers_pics/jbl_br-55.png" alt="Speaker model JBL BR-55">\
+        <p class="speakers__card-title">JBL BR-55</p>\
+        <p class="speakers__card-price">$140.00</p>\
+        <a class="speakers__card-link" href="#">ADD TO CART</a>\
+    </article>',
+  '<article class="speakers__card">\
+        <img class="speakers__card-image" src="assets/images/speakers_pics/jbl_gt-345.png" alt="Speaker model JBL GT-345">\
+        <p class="speakers__card-title">JBL GT-345</p>\
+        <p class="speakers__card-price">$120.00</p>\
+        <a class="speakers__card-link" href="#">ADD TO CART</a>\
+    </article>',
+  '<article class="speakers__card">\
+        <img class="speakers__card-image" src="assets/images/speakers_pics/jbl_or-57.png" alt="Speaker model JBL OR-57">\
+        <p class="speakers__card-title">JBL OR-57</p>\
+        <p class="speakers__card-price">$98.00</p>\
+        <a class="speakers__card-link" href="#">ADD TO CART</a>\
+    </article>',
+  '<article class="speakers__card">\
+        <img class="speakers__card-image" src="assets/images/speakers_pics/jbl_party_pt-25.png" alt="Speaker model JBL Party PT-25">\
+        <p class="speakers__card-title">JBL Party PT-25</p>\
+        <p class="speakers__card-price">$180.00</p>\
+        <a class="speakers__card-link" href="#">ADD TO CART</a>\
+    </article>',
+  '<article class="speakers__card">\
+        <img class="speakers__card-image" src="assets/images/speakers_pics/jbl_rr-56.png" alt="Speaker model JBL RR-56">\
+        <p class="speakers__card-title">JBL RR-56</p>\
+        <p class="speakers__card-price">$110.00</p>\
+        <a class="speakers__card-link" href="#">ADD TO CART</a>\
+    </article>',
+  '<article class="speakers__card">\
+        <img class="speakers__card-image" src="assets/images/speakers_pics/sony_yt555.png" alt="Speaker model Sony YT555">\
+        <p class="speakers__card-title">Sony YT555</p>\
+        <p class="speakers__card-price">$140.00</p>\
+        <a class="speakers__card-link" href="#">ADD TO CART</a>\
+    </article>',
+];
+
+/* Create a variable for the index of carousel elements */
+let currentSlideIndex = 0;
+
+/* The function of the carousel of the columns */
+function renderCarousel() {
+  const slideContainer = document.querySelector(".speakers-carousel__slides");
+  slideContainer.innerHTML = slides[currentSlideIndex];
+
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    const secondSlideIndex =
+      currentSlideIndex + 1 >= slides.length ? 0 : currentSlideIndex + 1;
+    slideContainer.innerHTML += slides[secondSlideIndex];
+
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+      const thirdSlideIndex =
+        secondSlideIndex + 1 >= slides.length ? 0 : secondSlideIndex + 1;
+      slideContainer.innerHTML += slides[thirdSlideIndex];
+
+      const fourthSlideIndex =
+        thirdSlideIndex + 1 >= slides.length ? 0 : thirdSlideIndex + 1;
+      slideContainer.innerHTML += slides[fourthSlideIndex];
+
+      const fifthSlideIndex =
+        fourthSlideIndex + 1 >= slides.length ? 0 : fourthSlideIndex + 1;
+      slideContainer.innerHTML += slides[fifthSlideIndex];
+    }
+  }
+}
+
+/* Function for the next slide */
+function nextSlide() {
+  currentSlideIndex++;
+  if (currentSlideIndex >= slides.length) currentSlideIndex = 0;
+  renderCarousel();
+}
+
+/* Function for displaying the previous slide */
+function prevSlide() {
+  currentSlideIndex =
+    currentSlideIndex - 1 < 0 ? slides.length - 1 : currentSlideIndex - 1;
+  renderCarousel();
+}
+
+renderCarousel();
+
+/* Gallery scroll buttons */
+const nextBtn = document.querySelector(".speakers-carousel__btn-next");
+nextBtn.addEventListener("click", nextSlide);
+const prevBtn = document.querySelector(".speakers-carousel__btn-prev");
+prevBtn.addEventListener("click", prevSlide);
+
+window.addEventListener("resize", renderCarousel);
