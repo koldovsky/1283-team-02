bc8749ed78b4e83cfd028bd567a5c6b6674994f2
koskin <118272123+koskin17@users.noreply.github.com>
2024-10-30 14:30:58 +0200

    add link for gallery to products pages

--- scripts/index.speakers.carousel.js
+++ scripts/index.speakers.carousel.js
@@ -3,37 +3,37 @@
 const slides = [
   '<article class="speakers__card">\
         <img class="speakers__card-image" src="assets/images/speakers_pics/jbl_br-55.png" alt="Speaker model JBL BR-55">\
-        <p class="speakers__card-title">JBL BR-55</p>\
+        <p class="speakers__card-title"><a href="product.html" class="link-to-product-page" data-id="jbl_br-55">JBL BR-55</a></p>\
         <p class="speakers__card-price">$140.00</p>\
         <a class="speakers__card-link" href="#">ADD TO CART</a>\
     </article>',
   '<article class="speakers__card">\
         <img class="speakers__card-image" src="assets/images/speakers_pics/jbl_gt-345.png" alt="Speaker model JBL GT-345">\
-        <p class="speakers__card-title">JBL GT-345</p>\
+        <p class="speakers__card-title"><a href="product.html" class="link-to-product-page" data-id="jbl_gt-345">JBL GT-345</a></p>\
         <p class="speakers__card-price">$120.00</p>\
         <a class="speakers__card-link" href="#">ADD TO CART</a>\
     </article>',
   '<article class="speakers__card">\
         <img class="speakers__card-image" src="assets/images/speakers_pics/jbl_or-57.png" alt="Speaker model JBL OR-57">\
-        <p class="speakers__card-title">JBL OR-57</p>\
+        <p class="speakers__card-title"><a href="product.html" class="link-to-product-page" data-id="jbl_or-57">JBL OR-57</a></p>\
         <p class="speakers__card-price">$98.00</p>\
         <a class="speakers__card-link" href="#">ADD TO CART</a>\
     </article>',
   '<article class="speakers__card">\
         <img class="speakers__card-image" src="assets/images/speakers_pics/jbl_party_pt-25.png" alt="Speaker model JBL Party PT-25">\
-        <p class="speakers__card-title">JBL Party PT-25</p>\
+        <p class="speakers__card-title"><a href="product.html" class="link-to-product-page" data-id="jbl_party_pt-25">JBL Party PT-25</a></p>\
         <p class="speakers__card-price">$180.00</p>\
         <a class="speakers__card-link" href="#">ADD TO CART</a>\
     </article>',
   '<article class="speakers__card">\
         <img class="speakers__card-image" src="assets/images/speakers_pics/jbl_rr-56.png" alt="Speaker model JBL RR-56">\
-        <p class="speakers__card-title">JBL RR-56</p>\
+        <p class="speakers__card-title"><a href="product.html" class="link-to-product-page" data-id="jbl_rr-56">JBL RR-56</a></p>\
         <p class="speakers__card-price">$110.00</p>\
         <a class="speakers__card-link" href="#">ADD TO CART</a>\
     </article>',
   '<article class="speakers__card">\
         <img class="speakers__card-image" src="assets/images/speakers_pics/sony_yt555.png" alt="Speaker model Sony YT555">\
-        <p class="speakers__card-title">Sony YT555</p>\
+        <p class="speakers__card-title"><a href="product.html" class="link-to-product-page" data-id="sony_yt555">Sony YT555</a></p>\
         <p class="speakers__card-price">$140.00</p>\
         <a class="speakers__card-link" href="#">ADD TO CART</a>\
     </article>',
